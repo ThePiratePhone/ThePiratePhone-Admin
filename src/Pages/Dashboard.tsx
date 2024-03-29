88820525efbc62154900bdf9771d51ea7ef1f47a
Wiwok <emil.dechenaud@gmail.com>
2024-03-29 11:03:05 +0100

    Added a temporary text on the main page

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -2,6 +2,9 @@ function Dashboard() {
 	return (
 		<div className="DashBoard">
 			<h1>Accueil</h1>
+			<div>
+				<h4>La page d'accueil est en cours de construction... Vous pouvez naviguer via la barre à gauche !</h4>
+			</div>
 		</div>
 	);
 }
